46c6a0f36d04c307a372c1b87256e5dd9f2d7df5
arhamdeveloper <marham.zain2204@gmail.com>
2024-10-08 18:19:27 +0500

    hello

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let a=prompt("enter your name" )
+let b=prompt("enter your age" )
+alert("hello " + a)
+
+document.write("Hello " + a + ". How are you. I am developer My name is Arham." )
+if(b<18){
+    alert
+    (a+",You can drink water beacuse you are under 18")
+}
+else{
+    alert
+    (a+",You can drink Coldrink because you are above 18")
+}
+
